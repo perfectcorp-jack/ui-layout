98e3bab86d2a10d05f5406ab003502e06910cb58
ChiaYuSu <jack879tw@gmail.com>
2022-10-27 21:51:46 +0800

    upadte but still some bugs

--- src/App.js
+++ src/App.js
@@ -4,71 +4,87 @@ import data from './data/product.json';
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      category: 'labiales',
+      subcat: '',
+      product: '',
+      color: '',
+      tonos: '',
+    }
+    this.handleCategory = this.handleCategory.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
+    this.handleColor = this.handleColor.bind(this);
+  }
+
+  handleCategory(event) {
+    this.setState({ category: event.target.value });
+  }
+
+  handleSelect(event) {
+    this.setState({ subcat: event.target.value });
+  }
+
+  handleColor(col) {
+    console.log(col);
+    console.log(this.state.product);
+    this.setState({ color: col, tonos: '' });
+  }
+
   render() {
-    console.log(data.labiales);
+    const { category, subcat, product } = this.state;
     return (
-      <div className="App">
-        <div style={{ textAlign: 'center' }}>
-          <h1>Labiales</h1>
-          <select>
-            {data.labiales.map((labial) => (
-              <option key={labial.id}>{labial.subcat}</option>
+      <div className="App" style={{ textAlign: 'center' }}>
+        {/* category */}
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          {Object.entries(data).map(([key, value]) => {
+            return (
+              <button style={{ width: '10vh', height: '3vh', marginRight: '2vh' }} value={key} onClick={this.handleCategory}>
+                {key}
+              </button>
+            )
+          })}
+        </div>
+
+        {/* subcategory */}
+        <div style={{ marginTop: '2vh' }}>
+          <select style={{ width: '10vh', height: '3vh' }} onChange={this.handleSelect}>
+            {data[category].map((labial) => (
+              <option key={labial.id} value={labial.subcat}>
+                {labial.subcat}
+              </option>
             ))}
           </select>
-          <div>
-            {data.labiales.map((labial) => (
-              <div key={labial.id}>
-                <h2>{labial.subcat}</h2>
-                {labial.product.map((products) => (
-                  <div key={products.id}>
-                    <h4>{products.name}</h4>
-                    <p>{products.desc}</p>
-                    <p>{products.price}</p>
-                    {products.tonos.map((tonos) => (
-                      <div key={tonos.id}>
-                        <span>{tonos}</span>
+        </div>
+
+        {/* product */}
+        <div>
+          {data[category].map((labial) => (
+            <div key={labial.id}>
+              {labial.subcat === subcat && (
+                <div style={{ textAlign: 'center' }}>
+                  <h1>{labial.subcat}</h1>
+                  {labial.product.map((products) => (
+                    <div key={products.id} style={{ border: '1px #999999 solid', margin: '2vh 10%' }}>
+                      <h5>{products.tonos}</h5>
+                      <h3>{products.name}</h3>
+                      <h5>{products.desc}</h5>
+                      <h5>{products.price}</h5>
+                      <div style={{ display: 'flex', justifyContent: 'center' }}>
+                        {products.color.map((col, id) => (
+                          <a onClick={() => this.handleColor(id)}>
+                            <div key={col.id} style={{ backgroundColor: col, width: '3vh', height: '3vh', marginRight: '.5vh' }}></div>
+                          </a>
+                        ))}
                       </div>
-                    ))}
-                    {products.color.map((colors) => (
-                      <div key={colors.id}>
-                        <span>{colors}</span>
-                      </div>
-                    ))}
-                  </div>
-                ))}
-              </div>
-            ))}
-          </div>
-          <h1>Máscaras</h1>
-          <select>
-            {data.Máscaras.map((mascara) => (
-              <option key={mascara.id}>{mascara.subcat}</option>
-            ))}
-          </select>
-          <div>
-            {data.Máscaras.map((mascara) => (
-              <div key={mascara.id}>
-                <h2>{mascara.subcat}</h2>
-                {mascara.product.map((products) => (
-                  <div key={products.id}>
-                    <h4>{products.name}</h4>
-                    <p>{products.desc}</p>
-                    <p>{products.price}</p>
-                    {products.tonos.map((tonos) => (
-                      <div key={tonos.id}>
-                        <span>{tonos}</span>
-                      </div>
-                    ))}
-                    {products.color.map((colors) => (
-                      <div key={colors.id}>
-                        <span>{colors}</span>
-                      </div>
-                    ))}
-                  </div>
-                ))}
-              </div>
-            ))}
-          </div>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+
         </div>
       </div>
     );
