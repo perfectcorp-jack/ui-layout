8504c20172b03ffcd46c7ddfa132b2060a7c2f1c
Jack Su <Jack_Su@PerfectCorp.com>
2022-10-28 14:38:08 +0800

    update

--- src/App.js
+++ src/App.js
@@ -8,34 +8,42 @@ class App extends React.Component {
     super(props);
     this.state = {
       category: 'labiales',
-      subcat: '',
+      subcat: 'Rojos',
       product: 0,
       color: 0,
       tonos: '',
     }
     this.handleCategory = this.handleCategory.bind(this);
-    this.handleSelect = this.handleSelect.bind(this);
+    this.handleSubcategory = this.handleSubcategory.bind(this);
     this.handleProduct = this.handleProduct.bind(this);
     this.handleColor = this.handleColor.bind(this);
   }
 
   handleCategory(event) {
     console.log(event.target.value);
-    this.setState({ category: event.target.value, product: 0 });
+    this.setState({ 
+      category: event.target.value, 
+      product: 0, 
+      subcat: data[event.target.value][0].subcat,
+    });
   }
 
-  handleSelect(event) {
-    this.setState({ subcat: event.target.value });
+  handleSubcategory(event) {
+    this.setState({ subcat: event.target.value, color: 0, product: 0 });
   }
 
   handleProduct(id) {
     console.log('product', id);
-    this.setState({ product: id });
+    this.setState({ product: id, color: 0 });
   }
 
   handleColor(col) {
     console.log(col);
-    this.setState({ color: col, tonos: '' });
+    console.log(data[this.state.category]);
+    this.setState({ 
+      color: col, 
+      tonos: '',
+    });
   }
 
   render() {
@@ -55,7 +63,7 @@ class App extends React.Component {
 
         {/* subcategory */}
         <div style={{ marginTop: '2vh' }}>
-          <select style={{ width: '10vh', height: '3vh' }} value={this.state.subcat} onChange={this.handleSelect}>
+          <select style={{ width: '10vh', height: '3vh' }} value={this.state.subcat} onChange={this.handleSubcategory}>
             {data[category].map((labial) => (
               <option key={labial.id} value={labial.subcat}>
                 {labial.subcat}
@@ -63,6 +71,7 @@ class App extends React.Component {
             ))}
           </select>
 
+          {/* product */}
           <div>
             {data[category].map((labial) => (
               <div key={labial.id}>
@@ -72,7 +81,7 @@ class App extends React.Component {
                     {labial.product.map((products, id) => (
                       <div>
                         <a onClick={() => this.handleProduct(id)}>
-                          <div key={products.id} style={{ border: '1px #999999 solid', margin: '2vh auto', width: '60vh' }}>
+                          <div key={products.id} style={{ border: '1px #999999 solid', margin: '2vh auto', width: '60vh' }} >
                             <h5>{products.tonos[color]}</h5>
                             <h3>{products.name}</h3>
                             <h5>{products.desc}</h5>
